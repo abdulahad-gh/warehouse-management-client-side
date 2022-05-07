6f07c973a7e67d0c9727cab45f587d7e64b3006e
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-05-07 17:51:15 +0600

    added client says section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
+import Client from './Client';
 import Items from './Items';
 
 const Home = () => {
     return (
         <>
             <Banner />
-
             <Items />
+            <Client />
 
 
         </>
--- src/pages/Home/Items.js
+++ src/pages/Home/Items.js
@@ -19,7 +19,7 @@ const Items = () => {
 
                             <div class="p-5">
 
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{laptop.name}</h5>
+                                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{laptop.name}</h5>
 
                                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{laptop.desc}.</p>
                                 <p class="mb-3 font-bold text-gray-700 dark:text-gray-400">Price: {laptop.price} tk</p>
