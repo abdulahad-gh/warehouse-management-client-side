65f7c8ec4a49ad8dceb39ff8b04352d5947e554c
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-05-08 17:22:11 +0600

    added firebase config,but when work with env get auth invalid error,now firebase config without env.and also added googleSignin

--- src/pages/Auth/Signin.js
+++ src/pages/Auth/Signin.js
@@ -5,11 +5,13 @@ import { toast } from 'react-toastify';
 import auth from '../../firebase-init';
 
 const Signin = () => {
+    const [signWithEmailAndPass, , , hookError] = useSignInWithEmailAndPassword(auth)
+    const [signWithGoogle, , , googleError] = useSignInWithGoogle(auth)
+
     const [userLogInInfo, setUserLogInInfo] = useState({
         email: '',
         password: ''
     })
-    const [signWithEmailAndPass, , , hookError] = useSignInWithEmailAndPassword(auth)
     const [errors, setErrors] = useState({
         email: '',
         password: '',
@@ -54,7 +56,7 @@ const Signin = () => {
 
     }
     useEffect(() => {
-        const error = hookError
+        const error = hookError || googleError
         if (error) {
             switch (error?.code) {
                 case "auth/invalid-email":
@@ -96,7 +98,12 @@ const Signin = () => {
                 <div>
                     <span>already account? please <Link to='signup'>SignUp</Link></span>
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">SignIn</button>
+                <button type="submit" class="mx-auto block mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">SignIn</button>
+                <br />
+                <button onClick={() => signWithGoogle()} type="button" class="w-full text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                    <svg class="w-4 h-4 mr-2 -ml-1 " aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
+                    Sign in with Google
+                </button>
             </form>
 
         </div>
